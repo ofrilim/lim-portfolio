9f5a3b6ca518a656ef9d1fa097987804902a46ea
Limor Ofri <limorofri7@gmail.com>
2020-06-20 15:50:59 +0300

    add a download resume button

--- js/proj-service.js
+++ js/proj-service.js
@@ -8,7 +8,7 @@ const gProjs = [
     createProj(id(), 'Appsus', 'Email & Notes app', 'Team work. My part was the Email app',
     'img/portfolio/frame-email.png', 'November 2019', ['HTML, CSS, JS, Vue'], 'https://ofrilim.github.io/appSus/#/email/inbox'),    
     createProj(id(), 'Meme Creator', 'Create your own meme!', 'Design you own meme!',
-    'img/portfolio/frame-meme.png', 'September 2019',  ['HTML, CSS, JS'], 'https://ofrilim.github.io/Meme-Creator/'),    
+    'img/portfolio/frame-meme.png', 'September 2019',  ['HTML, canvas, CSS, JS'], 'https://ofrilim.github.io/Meme-Creator/'),    
     createProj(id(), 'B-Good', 'E-commerce fashion app', 'Buying and selling clothes online',
     'img/portfolio/frame-bgood.png', 'October 2019', ['Vue, JS, SASS, Node.js, mongoDB'], 'https://b-good.herokuapp.com/'),    
     // createProj(id(), 'Mine Sweaper', 'My first self Project!', 'super cool mine sweaper game!',
