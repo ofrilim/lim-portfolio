4c9d2d2cce070533ebbd5af5aa2565d8104d6977
ofrilim <ofrilim@gmail.com>
2021-04-27 17:48:00 +0300

    update CV file and added elevators project

--- js/main.js
+++ js/main.js
@@ -42,13 +42,15 @@ function renderModal(projId) {
     const strHtml = `<div class="portfolio-modal modal fade" id="portfolioModal" tabindex="-1" role="dialog" aria-hidden="true">
                     <div class="modal-dialog"><div class="modal-content"><div class="close-modal" data-dismiss="modal">
                     <div class="lr"><div class="rl"></div></div></div>
-                    <div class="container"><div class="row"><div class="col-lg-8 mx-auto"><div class="modal-body">
+                    <div class="container"><div class="row"><div class="col-lg-6 mx-auto"><div class="modal-body">
                     <h2>${currProj.name}</h2><p class="item-intro text-muted">${currProj.title}</p>
                     <img class="img-fluid d-block mx-auto" src="${currProj.src}" alt=""><p>${currProj.desc}</p>
                     <ul class="list-inline"><li>Date: ${currProj.publishedAt}</li><li>Technologies: ${currProj.labels}</li>
                     </ul><button class="btn btn-primary" type="button">
-                    <a href="${currProj.url}" target="blank">visit website</a>
-                    </button><button class="btn btn-primary" data-dismiss="modal" type="button">
+                    <a href="${currProj.websiteUrl}" target="blank">visit website</a>
+                    </button><button class="btn btn-primary" type="button">
+                    <a href="${currProj.codeUrl}" target="blank">code</a>
+                    </button><button class="btn btn-primary d-block" style="margin: 0.5rem auto" data-dismiss="modal" type="button">
                     <i class="fa fa-times"></i>back</button></div></div></div></div></div></div></div>`
 
     $('.modal-container').html(strHtml);
--- js/proj-service.js
+++ js/proj-service.js
@@ -5,17 +5,19 @@ const id = (function() {
 })()
 
 const gProjs = [
+    createProj(id(), 'Mine Sweaper', 'My first self Project!', 'A traditional mine sweaper game with option to choose a level',
+    'img/portfolio/frame-minesweaper.png', 'September 2019', ['Html, CSS, JS'], 'https://ofrilim.github.io/mine-sweeper/', 'https://github.com/ofrilim/mine-sweeper'),
+    createProj(id(), 'Elevators System', 'Manage calls to elevators in a building', 'A project that I developed during learning react.js',
+    'img/portfolio/frame-elevators.png', 'April 2021', ['React.js, SCSS'], 'https://ofrilim.github.io/elevator-react/', 'https://github.com/ofrilim/elevator-react'),
     createProj(id(), 'Appsus', 'Email & Notes app', 'Team work. My part was the Email app',
-    'img/portfolio/frame-email.png', 'November 2019', ['HTML, CSS, JS, Vue'], 'https://ofrilim.github.io/appSus/#/email/inbox'),    
-    createProj(id(), 'Meme Creator', 'Create your own meme!', 'Design your own meme!',
-    'img/portfolio/frame-meme.png', 'September 2019',  ['HTML, canvas, CSS, JS'], 'https://ofrilim.github.io/Meme-Creator/'),    
+    'img/portfolio/frame-email.png', 'November 2019', ['Html, CSS, JS, Vue'], 'https://ofrilim.github.io/appSus/#/email/inbox', 'https://github.com/ofrilim/appSus/tree/master/js/apps/missEmail'),    
+    createProj(id(), 'Meme Creator', 'Create your own meme!', 'Design your own meme, option to upload your own image or choose from gallery!',
+    'img/portfolio/frame-meme.png', 'September 2019',  ['Html, canvas, CSS, JS'], 'https://ofrilim.github.io/Meme-Creator/', 'https://github.com/ofrilim/Meme-Creator'),    
     createProj(id(), 'B-Good', 'E-commerce fashion app', 'Buying and selling clothes online',
-    'img/portfolio/frame-bgood.png', 'October 2019', ['Vue, JS, SASS, Node.js, mongoDB'], 'https://b-good.herokuapp.com/'),    
-    // createProj(id(), 'Mine Sweaper', 'My first self Project!', 'super cool mine sweaper game!',
-    //  'img/portfolio/photo-frame-computer+mine.png', 'September 2019', ['html, css, JS'], ''),
+    'img/portfolio/frame-bgood.png', 'October 2019', ['Vue, Vuex, SCSS, Node.js, Express, mongoDB'], 'https://b-good.herokuapp.com/', 'https://github.com/ofrilim/Bgood/tree/master/frontend/src')
 ];
 
-function createProj(id, name, title, desc, src, publishedAt, labels, url) {
+function createProj(id, name, title, desc, src, publishedAt, labels, websiteUrl, codeUrl) {
     return {
         id,
         name,
@@ -24,7 +26,8 @@ function createProj(id, name, title, desc, src, publishedAt, labels, url) {
         src,
         publishedAt,
         labels,
-        url 
+        websiteUrl,
+        codeUrl
     }
 }
 
